b45fbca1c173b7d0143585b4f3ac9f8f853933db
vasa3571 <vasilkravchuk23@gmail.com>
2025-04-30 22:08:47 +0300

    Add files via upload

---
+++ mysite/app_blog/forms.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from django import forms
+from .models import ArticleImage
+
+
+class ArticleImageForm(forms.ModelForm):
+    image = forms.ImageField(
+        widget=forms.ClearableFileInput()
+    )
+
+    class Meta:
+        model = ArticleImage
+        fields = '__all__'
